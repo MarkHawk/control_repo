998d340ca8be0be86ce56d614e821c913d718543
MarkHawk <53855765+MarkHawk@users.noreply.github.com>
2019-10-15 12:10:44 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,3 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'this is a stupid readme',
-    owner   => 'root',
-  }
-  file {'/root/README':
-  owner   => 'root',
-  }
+  
 }
