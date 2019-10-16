3da25f99a54a1f10c7f77c4836e1078b73d9417e
MarkHawk <53855765+MarkHawk@users.noreply.github.com>
2019-10-16 12:21:13 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -6,7 +6,7 @@ class minecraft {
       ensure => file,
     source => 'https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar'
 }
-pakage {'java':
+package {'java':
   ensure => present,
  }
   file {'/opt/minecraft/eula.txt':
